e0d8ea22b23b62e98e15e3c43c6140b48ea5e3a9
Dilen Govin <dilen.govin@avanacompanies.com>
2026-03-13 11:07:31 -0700

    feat: add routing

--- campaign-manager/src/app/app.routes.ts
+++ campaign-manager/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/pages/login/login.component';
+import { RegisterComponent } from './features/auth/pages/register/register.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'login', pathMatch: 'full'},
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'dashboard', component: DashboardComponent },
+];
