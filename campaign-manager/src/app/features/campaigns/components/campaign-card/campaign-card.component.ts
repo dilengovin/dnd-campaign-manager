ce5c55b7d855611c3a3ce4f24c002c8c12d2ed8c
Dilen Govin <dilen.govin@avanacompanies.com>
2026-03-13 11:40:12 -0700

    upgraded ng 18 -> ng 21, add tailwindcss

--- campaign-manager/src/app/features/campaigns/components/campaign-card/campaign-card.component.ts
+++ campaign-manager/src/app/features/campaigns/components/campaign-card/campaign-card.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-campaign-card',
-  standalone: true,
-  imports: [],
-  templateUrl: './campaign-card.component.html',
-  styleUrl: './campaign-card.component.scss'
+    selector: 'app-campaign-card',
+    imports: [],
+    templateUrl: './campaign-card.component.html',
+    styleUrl: './campaign-card.component.scss'
 })
 export class CampaignCardComponent {
 
